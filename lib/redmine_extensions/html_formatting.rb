4972f9f2929fc8dbad704e71bcf22e235ac049cb
ezro <ondrej.ezr@easy.cz>
2015-07-13 02:17:16 +0200

    after init

--- lib/redmine_extensions/html_formatting.rb
+++ lib/redmine_extensions/html_formatting.rb
@@ -2,6 +2,6 @@ require 'redmine_extensions/html_formatting/internals'
 require 'redmine_extensions/html_formatting/formatter'
 require 'redmine_extensions/html_formatting/helper'
 
-ActiveSupport.on_load(:redmine) do
+Rails.application.after_initialize do
   Redmine::WikiFormatting.register(:HTML, RedmineExtensions::HTMLFormatting::Formatter, RedmineExtensions::HTMLFormatting::Helper)
 end
