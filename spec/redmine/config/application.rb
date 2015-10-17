38ebaf7225d30ed9380ef85750b1690d17ed37a9
ezro <ondrej.ezr@easy.cz>
2015-10-17 13:03:05 +0200

    testing under redmine and easyredmin

--- spec/redmine/config/application.rb
+++ spec/redmine/config/application.rb
@@ -3,7 +3,6 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-require "redmine_extensions"
 
 module RedmineApp
   class Application < Rails::Application
