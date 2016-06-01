32118f1bb635dcd6c22e359ccedcca95e039daa5
Ondřej Ezr <ezrondre@fit.cvut.cz>
2016-06-01 14:01:24 +0200

    cleanup for presenter

--- lib/redmine_extensions/query_outputs/tile_output.rb
+++ lib/redmine_extensions/query_outputs/tile_output.rb
@@ -14,19 +14,6 @@ module RedmineExtensions
         referenced_entity.to_s
       end
 
-      def render_data
-        h.content_tag(:div, class: 'easy-entity-cards-container') do
-          h.content_tag(:div, class: 'splitcontent') do
-            query.entities.map do |referenced_entity|
-              render_entity_card(referenced_entity)
-            end.join('').html_safe
-          end
-        end
-      end
-
-      def render_period_header
-      end
-
     end
   end
 end
