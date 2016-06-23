c81b177cabc717256bc140112cd89c5bb7dd0ff7
Ondřej Ezr <ezrondre@fit.cvut.cz>
2016-06-23 15:53:03 +0200

    bundle update

--- .run-tests.sh
+++ .run-tests.sh
@@ -36,7 +36,7 @@ ruby -ryaml -rsecurerandom -e "
   File.write('config/database.yml', config)
 "
 
-bundle install
+bundle update
 bundle exec rake db:drop
 bundle exec rake db:create
 bundle exec rake db:migrate
