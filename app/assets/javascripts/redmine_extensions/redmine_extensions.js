e55e5195225426f8e937ed4773be61edddd23cd6
Ondřej Ezr <ezrondre@fit.cvut.cz>
2016-07-14 14:30:28 +0200

    result not return

--- app/assets/javascripts/redmine_extensions/redmine_extensions.js
+++ app/assets/javascripts/redmine_extensions/redmine_extensions.js
@@ -585,16 +585,16 @@ window.closeFlashMessage = (function($element){
         getValue: function(with_label) {
             var result;
             if ( this.options.multiple && !this.expanded ) {
-              result = this.valueElement.entityArray('getValue'); // entityArray
+                result = this.valueElement.entityArray('getValue'); // entityArray
             } else if ( this.options.multiple ) {
-                return this.valueElement.val(); // select multiple=true
+                result = this.valueElement.val(); // select multiple=true
             } else {
-              result = [this.valueElement.val()]; // hidden field
+                result = [this.valueElement.val()]; // hidden field
             }
             if( with_label ) {
-              result = this.possibleValues.filter(function(el) {
-                return result.indexOf( el.id ) >= 0;
-              });
+                result = this.possibleValues.filter(function(el) {
+                    return result.indexOf( el.id ) >= 0;
+                });
             }
             return result;
         }
