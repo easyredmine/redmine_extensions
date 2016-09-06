ba554fd3d6ca3066d40fcda15ef9a631429aa77f
Ondřej Ezr <ezrondre@fit.cvut.cz>
2016-09-06 12:24:39 +0200

    write files in one string

--- spec/support/plugin_generator.rb
+++ spec/support/plugin_generator.rb
@@ -22,10 +22,12 @@ module PluginGenerator
 
   def self.generate_autocomplete_controller!
     File.open(Rails.root.join('plugins', 'dummy_plugin', 'app', 'controllers', 'dummy_autocompletes_controller.rb'), 'w') do |file|
-      file.write("class DummyAutocompletesController < ApplicationController\n")
-      file.write("  def index\n")
-      file.write("  end\n")
-      file.write("end\n")
+      file.write( <<-END_RUBY )
+        class DummyAutocompletesController < ApplicationController
+          def index
+          end
+        end
+      END_RUBY
     end
   end
 
@@ -39,9 +41,11 @@ module PluginGenerator
     dir = Rails.root.join('plugins', 'dummy_plugin', 'app', 'views', 'dummy_autocompletes')
     Dir.mkdir dir
     File.open(dir.join('index.html.erb'), 'w') do |file|
-      file.write("<%= form_tag('/dummy_autocompletes', id: 'autocompletes_form') do %>\n")
-      file.write("  <%= autocomplete_field_tag('default', ['value1', 'value2'], ['value1']) %>\n")
-      file.write('<% end %>')
+      file.write( <<-END_RUBY )
+        <%= form_tag('/dummy_autocompletes', id: 'autocompletes_form') do %>
+          <%= autocomplete_field_tag('default', ['value1', 'value2'], ['value1']) %>
+        <% end %>
+      END_RUBY
     end
   end
 end
