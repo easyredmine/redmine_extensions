4215898c32239633c2b28903b7aa66f118d45d9b
Petr Hošek <ringael@seznam.cz>
2018-05-17 17:59:45 +0200

    refs #244427 - fix missing redmine JS files

--- lib/redmine_extensions/hooks.rb
+++ lib/redmine_extensions/hooks.rb
@@ -29,15 +29,16 @@ module RedmineExtensions
         ## BLOCKING ##
         javascript_include_tag('redmine_extensions/blocking_namespace') +
           javascript_include_tag('redmine_extensions/blocking_schedule') +
+          javascript_include_tag('redmine_extensions/blocking_module') +
           javascript_include_tag('redmine_extensions/blocking_polyfill') +
           javascript_include_tag('redmine_extensions/blocking_render') +
           javascript_include_tag('redmine_extensions/blocking_utils') +
+          javascript_include_tag('redmine_extensions/blocking_dynamic_loading') +
 
           ## DEFERRED ##
           javascript_include_tag('redmine_extensions/easy_togglers') +
           javascript_include_tag('redmine_extensions/jquery.entityarray') +
           javascript_include_tag('redmine_extensions/render_polyfill') +
-          javascript_include_tag('redmine_extensions/dynamic_loading') +
           javascript_include_tag('redmine_extensions/redmine_extensions')
       end
     end
